03c53f8eace469e6ea9c80c743aa61b3bd8e1d0b
jairocomino <jairocominoruiz@gmail.com>
2022-10-20 19:39:38 +0200

    ejercicio6 jairo

---
+++ ejercicios/ejercicio6.js
@@ -0,0 +1,14 @@
+/**
+ * 6 Modificar la función anterior para que redondee el precio del IVA con 3 decimales.
+ * 
+ */
+ function precioiva3(IVA){
+    const copia = structuredClone(estudiantes);
+    copia=copia.equipo.precio*(IVA/100)+1;
+    copia= Math.pow(copia.equipo.precio);
+    return copia;
+    console.log(copia.precio);
+}
+
+
+
