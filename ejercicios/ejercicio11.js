d413dc47d6cf298ed66445177ef29eb650f8737e
sofiaKhud <saiihara3107@gmail.com>
2022-10-20 19:33:01 +0200

    ejercicio 11

---
+++ ejercicios/ejercicio11.js
@@ -0,0 +1,21 @@
+/**
+ * 11 -  Modificar la función anterior para convertirla en forma de
+programación declarativa
+@Author: Sofia
+ */
+
+let arrayNum = [2,3,4,5,6,7,2,2,2];
+let numero = 2;
+let newArray = [];
+
+function ejercicio10(num,numMult){
+    if(num <= 0 || isNaN(num)){
+        console.log("no es un número válido");
+    }else{
+        for(let i=0; i<arrayNum.length; i++){
+            let mult = arrayNum[i]*numMult;
+            console.log(mult);
+        }
+    }
+}
+console.log(ejercicio10(8,33));
